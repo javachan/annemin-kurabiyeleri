62fc09cf9b9957407afb2afd12100a6885f565fb
Mehmet Yilmaz <m.yilmaz0793@gmail.com>
2018-04-10 23:44:12 +0300

    arayüze geçildi

--- anneminkurabiyeleri/src/main/java/com/yilmazmehmet/anneminkurabiyeleri/controller/PageController.java
+++ anneminkurabiyeleri/src/main/java/com/yilmazmehmet/anneminkurabiyeleri/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.yilmazmehmet.anneminkurabiyeleri.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,6 +14,26 @@ public ModelAndView index(){
 	
 	ModelAndView mv = new ModelAndView("page");
 	mv.addObject("greeting","Welcome to Spring Web Mvc");
+	return mv;
+}
+/*
+@RequestMapping(value="/test")
+public ModelAndView test(@RequestParam(value="greating",required=false)String greating){
+	if(greating==null){
+		greating="Hello There";
+	}
+	ModelAndView mv = new ModelAndView("page");
+	mv.addObject("greeting",greating);
+	return mv;
+}*/
+
+@RequestMapping(value="/test/{greating}")
+public ModelAndView test(@PathVariable("greating")String greating){
+	if(greating==null){
+		greating="Hello There";
+	}
+	ModelAndView mv = new ModelAndView("page");
+	mv.addObject("greeting",greating);
 	return mv;
 }
 }
